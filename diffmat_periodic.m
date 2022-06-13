48a9fa5ffdccbff3f35576b9a222ccd1dd37b081
Mengjian Hua <mh5113@nyu.edu>
2022-06-13 17:09:34 -0400

    fixed the code that implemented periodic BCs

--- diffmat_periodic.m
+++ diffmat_periodic.m
@@ -1,4 +1,4 @@
-function D = diffmat_periodic(N,order)
+function [D,B] = diffmat_periodic(N,order)
 
 % This function creates a Chebyshev differentation matrix with periodic
 % boundary conditions. (i.e. function values and first derivatives match
@@ -12,5 +12,12 @@ D(1,1) = 1;
 D(1,end) = -1;
 D(end,:) = D1(1,:) - D1(end,:);
 
+B = zeros(N,N);
+B(2:end-1,2:end-1) = eye(N-2);
+
 
 end
+
+%% test code
+% [D,B] = diffmat_periodic(100,2);
+% lambda = flip(sort(eig(D,B)));
