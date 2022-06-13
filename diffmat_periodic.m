af0e1e2d2e237257e870ec92e6a3114e9c95242d
Mengjian Hua <mh5113@nyu.edu>
2022-06-13 15:43:00 -0400

    fixed the bug about matching the first derivatives

--- diffmat_periodic.m
+++ diffmat_periodic.m
@@ -1,19 +1,16 @@
-function D = diffmat_periodic(N)
+function D = diffmat_periodic(N,order)
 
 % This function creates a Chebyshev differentation matrix with periodic
 % boundary conditions. (i.e. function values and first derivatives match
 % at the two endpoints)
 % Installation of Chebfun is required. 
 
-D = diffmat(N,2);
-D1 = D(1,:);
+D = diffmat(N,order);
+D1 = diffmat(N,1);
 D(1,:) = 0;
 D(1,1) = 1;
 D(1,end) = -1;
-D(end,:) = D1 - D(end,:);
-
-
-
+D(end,:) = D1(1,:) - D1(end,:);
 
 
 end
