b81128098901c3fff6d5f06917099c3315d2353b
Mengjian Hua <mh5113@nyu.edu>
2022-06-23 11:28:25 -0400

    completed task #4

---
+++ null.m
@@ -0,0 +1,22 @@
+function x = null(A)
+
+%% Solve for the singular linear system Ax = b
+%  based on the randomized method in the paper by
+%  Sifuentes, Gimbutas, & Greengard
+%  https://arxiv.org/pdf/1401.3068.pdf
+
+dim = size(A);
+p = randn(dim(1),1);
+q = randn(dim(1),1);
+
+x = randn(dim(1),1);
+b = A*x;
+y = (A + p*q')\b;
+x = x-y;
+
+
+
+
+
+
+end
