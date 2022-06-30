f48a377658e206aadaa1ed6698f4ce523c942cca
Mengjian Hua <mh5113@nyu.edu>
2022-06-30 15:18:58 -0400

    added some comments

--- task4/inv_power.m
+++ task4/inv_power.m
@@ -1,19 +1,21 @@
 %% inverse power iteration for spherical harmonics
 clear all;
-l = 16;
-n = 20;
+l = 16; % number of eigenvalues we want to compute
+n = 20; % number of mesh points
 dom = surfacemesh.sphere(n, 2);
 
 pdo = [];
 pdo.lap = 1;
 L = surfaceop(dom, pdo, 0);
-tol = 1e-3;
+tol = 1e-3; % tolerance used to stop the iteration
 
+% build a random orthonormal basis 
 for i = 1:l
     Q(:,i) = surfacefun(@(x,y,z) sin(i*x)+cos(i*y), dom);
 end
 Q = orth(orth(Q));
 
+% first iteration
 tic
 for i = 1:l
     L = updateRHS(L, Q(:,i));
@@ -21,9 +23,10 @@ for i = 1:l
 end
 
 [Q,R] = qr(Z);
-num_iter = 1;
+num_iter = 1; % number of iterations
 R_previous = zeros(size(R));
 
+% subsequent iterations
 while(trace(abs(R-R_previous))>tol)
     
     for i = 1:l
