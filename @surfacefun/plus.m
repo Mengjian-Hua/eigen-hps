fa13f520c2ea0d9bdba7ebe14a02fd5c9924bfda
Mengjian Hua <mh5113@nyu.edu>
2022-06-29 21:43:22 -0400

    rewrote some subroutines and checked spherical harmonics

--- @surfacefun/plus.m
+++ @surfacefun/plus.m
@@ -16,7 +16,22 @@ elseif ( isnumeric( g ) )
 elseif ( isa(f, 'surfacefun') && isa(g, 'surfacefun') )
     h = f;
     % TODO: Assume on the same grid for now.
-    h.vals = cellfun(@plus, f.vals , g.vals, 'UniformOutput', false);
+    [~,m] = size(f);
+    [~,n] = size(g);
+    if(m==n)
+        for i = 1:m
+            h(:,i).vals = cellfun(@plus, f(:,i).vals , g(:,i).vals, 'UniformOutput', false);
+        end
+    elseif(m>1 && n==1)
+        for i = 1:n
+            h(:,i).vals = cellfun(@plus, f(:,i).vals , g.vals, 'UniformOutput', false);
+        end
+    elseif(m == 1 && n>1)
+        h = plus(g,f);
+    else
+        error('SURFACEFUN:mtimes:invalid', 'dimensions of f and g are not compatible.')
+    end
+%     h.vals = cellfun(@plus, f.vals , g.vals, 'UniformOutput', false);
 end
 
 end
---
+++ task4/check_sol.m
@@ -0,0 +1,27 @@
+clear all;
+l = 10;
+n = 20;
+dom = surfacemesh.sphere(n, 2);
+
+for i = 1:2*l+1
+    
+    sol = spherefun.sphharm(l,i-l-1);
+    sol = surfacefun(@(x,y,z) sol(x,y,z), dom);
+    A(:,i) = sol;
+    
+end
+
+pdo = [];
+pdo.lap = 1;
+pdo.b = l*(l+1);
+tic
+L = surfaceop(dom, pdo, 0);
+Z = null(L);
+Z = orth(Z);
+for i = 1: 2*l+1
+    c = A\Z(:,i);
+    Z1 = A*c;
+    d = Z1 - Z(:,i);
+    e(i) = norm(d);
+end
+toc
