5fbe272fdfcd68d6267e8b6445069952f7c05280
Mengjian Hua <mh5113@nyu.edu>
2022-06-30 15:18:58 -0400

    added some comments

--- task4/check_sol.m
+++ task4/check_sol.m
@@ -3,6 +3,7 @@ l = 5;
 n = 20;
 dom = surfacemesh.sphere(n, 2);
 
+% build the true solution, which is spherical harmonics 
 for i = 1:2*l+1
     
     sol = spherefun.sphharm(l,i-l-1);
@@ -11,13 +12,19 @@ for i = 1:2*l+1
     
 end
 
+% construct the operator 
 pdo = [];
 pdo.lap = 1;
 pdo.b = l*(l+1);
+
 tic
+
+% compute the orthonormal eigenfunctions 
 L = surfaceop(dom, pdo, 0);
 Z = null(L);
-Z = orth(Z);
+Z = orth(orth(Z));
+
+% compare coefficients
 for i = 1: 2*l+1
     c = A\Z(:,i);
     Z1 = A*c;
